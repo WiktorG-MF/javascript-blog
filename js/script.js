5023ca7d095717411bac03f400c5ad7ed1ecd7ef
Wiktor G <wiktor.gebka@gmail.com>
2021-03-30 22:47:30 +0200

    create methods to add .active in appropriate article

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked')
   
@@ -30,13 +31,21 @@ console.log('clickedElement:', clickedElement);
 
 //   get 'href' attribute from the clicked link
 
+let articleSelector = clickedElement.getAttribute('href');
+console.log(articleSelector);
+
 //   find the correct article using the selector (value of 'href' attribute)
+
+let correctArticle = document.querySelector(articleSelector);
+console.log(correctArticle);
 
 //   add class 'active' to the correct acticle
 
+correctArticle.classList.add('active');
+
 }
-  const links = document.querySelectorAll('.titles a');
+const links = document.querySelectorAll('.titles a');
   
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
