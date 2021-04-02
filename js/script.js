c0c6b0041dd56cd940dfa5691b8090a7abcd4508
Wiktor G <wiktor.gebka@gmail.com>
2021-04-03 01:27:56 +0200

    fill task runner [task runner for js]

--- js/script.js
+++ js/script.js
@@ -2,33 +2,33 @@
 
 function titleClickHandler(event){
   event.preventDefault();
-    const clickedElement = this;
-    console.log('Link was clicked')
+  const clickedElement = this;
+  console.log('Link was clicked');
 
-const activeLinks = document.querySelectorAll('.titles a.active');
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
-  console.log("pętla usunięcie");
-  activeLink.classList.remove('active');
-}
+  for(let activeLink of activeLinks){
+    console.log('pętla usunięcie');
+    activeLink.classList.remove('active');
+  }
 
-const activeArticles = document.querySelectorAll('.post');
+  const activeArticles = document.querySelectorAll('.post');
 
-for(let activeArticle of activeArticles){
-  activeArticle.classList.remove('active');
-}
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
-console.log('clickedElement:', clickedElement);
-clickedElement.classList.add('active');
-console.log('clickedElement:', clickedElement);
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
 
-let articleSelector = clickedElement.getAttribute('href');
-console.log(articleSelector);
+  let articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
-let correctArticle = document.querySelector(articleSelector);
-console.log(correctArticle);
+  let correctArticle = document.querySelector(articleSelector);
+  console.log(correctArticle);
 
-correctArticle.classList.add('active');
+  correctArticle.classList.add('active');
 }
 
 // Generate Title Links
@@ -67,10 +67,10 @@ function generateTitleLinks(){
 
   titleList.innerHTML = html;
 
-    const links = document.querySelectorAll('.titles a');
+  const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
 }
-}
-  generateTitleLinks();
+generateTitleLinks();
